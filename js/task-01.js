9880a8ee3000591fa1c6c2cdb6673fa9fec3006d
Inna1312 <126244200+Inna1312@users.noreply.github.com>
2023-07-04 22:44:25 +0300

    homework6

--- js/task-01.js
+++ js/task-01.js
@@ -1,10 +1,9 @@
-const categoriesList = document.querySelectorAll("ul#categories");
-const totalElements = categoriesList.children.length;
-console.log(`Number of categories: ${totalElements}`);
-categoriesList.forEach((category) => {
-    const categoryName = category.querySelector("h2").textContent;
-    const categoryElements = category.querySelectorAll("li").length;
-    console.log('Category : ${categoryName}');
-    console.log('Elements: ${categoryElements}');
-  }
-  );
+const categoryItems = document.querySelectorAll('.item');
+const itemsCount = categoryItems.length;
+console.log(`Number of categories: ${ itemsCount }`);
+categoryItems.forEach(category => {
+  const categoryName = category.querySelector('h2').textContent;
+  const categoryElements = category.querySelectorAll('li').length;
+  console.log(`Category: ${categoryName}`);
+  console.log(`Elements: ${categoryElements}`);
+});
